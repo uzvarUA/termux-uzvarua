79ee455c32733b976d27824e15600e1a9464a579
Uzvar <136013537+uzvarUA@users.noreply.github.com>
2025-09-04 22:12:18 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+echo "UzvarUa"
+echo "Виберіть модуля"
+echo "1) оновлення пакетів"
+echo "2) Встановлення proot python tur-repo"
+echo "3) Встановлення thc-hydra make mandoc"
+echo "4)  Встановлення libjpeg-turbo libpng zlib"
+echo "5) Встановлення openssh rust"
+echo "6) Встановлення ffmpeg"
+echo "7) Встановлення yt-dlp"
+echo "8) Вийти"
+
+read -p "Виберіть опцію: " opt
+case "$opt" in
+	1) bash module/update.sh ;;
+	2) bash module/proot_python_tur-repo.sh ;;
+	3) bash module/thc-hydra_make_mandoc.sh ;;
+	4) bash module/libjpeg-turbo_libpng_zlib.sh ;;
+	5) bash module/openssl_rust.sh ;;
+	6) bash module/ffmpeg.sh ;;
+	7) bash module/yt-dlp.sh ;;
+	8) echo "Вийти" && exit ;;
+	*) echo "Не правильно"
+esac
