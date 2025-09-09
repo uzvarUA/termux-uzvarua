72a7c4a5029da3221266b62ca9ece30dc1abe437
Uzvar <136013537+uzvarUA@users.noreply.github.com>
2025-09-09 17:03:49 +0300

    Update findutils.sh

--- module/findutils.sh
+++ module/findutils.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 clear
-echo "Встановлення findutils
-pkg install findutils -y
+echo "Встановлення findutils"
+pkg install findutils which -y
 echo "Готово"
