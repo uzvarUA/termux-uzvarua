199c06305e16a55d6d7ae15349eac9655eed9fbb
Uzvar <136013537+uzvarUA@users.noreply.github.com>
2025-09-13 19:45:54 +0300

    Update x11-repo.sh

--- module/x11-repo.sh
+++ module/x11-repo.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo "Встановлення"
+echo -e "\033[1;34m🚀 Починаємо встановлення...\033[0m"
 { pkg install x11-repo -y; pkg install termux-x11-nightly -y; pkg install xfce4 -y; } || { echo "Не встановлено"; exit }
 echo "✅ Готово"
