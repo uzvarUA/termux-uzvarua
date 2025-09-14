982ac08bc479f68021b3d3ecf943072752e0860c
Uzvar <136013537+uzvarUA@users.noreply.github.com>
2025-09-14 10:52:48 +0300

    Update x11-repo.sh

--- module/x11-repo.sh
+++ module/x11-repo.sh
@@ -4,7 +4,8 @@ echo -e "\033[1;34m🚀 Починаємо встановлення...\033[0m"
 
 if pkg install x11-repo -y && \
    pkg install termux-x11-nightly -y && \
-   pkg install xfce4 -y; then
+   pkg install xfce4 -y && \
+   pkg install pulseaudio -y; then
   echo -e "\033[1;32m✅ Готово!\033[0m"
 else
   echo -e "\033[1;31m❌ Не вдалося встановити пакети.\033[0m"
